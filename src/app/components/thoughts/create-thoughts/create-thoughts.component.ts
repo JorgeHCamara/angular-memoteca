274a5ec7400db27a77b047de185b30f069677d13
JorgeHCamara <jorgehcamara@hotmail.com>
2023-07-20 23:39:51 -0300

    chore: create-thoughts component property binding implementend

--- src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
+++ src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-thoughts',
   templateUrl: './create-thoughts.component.html',
   styleUrls: ['./create-thoughts.component.css']
 })
-export class CreateThoughtsComponent {
+export class CreateThoughtsComponent implements OnInit {
+
+  thought = {
+    id: '1',
+    content: 'Learning Angular',
+    author: 'Dev',
+    template: ''
+  }
+
+  constructor () {}
+
+  ngOnInit(): void {
+
+  }
 
 }
