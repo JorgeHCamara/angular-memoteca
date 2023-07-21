be9a6cab2b624edf2eee255f5ffa60a3b1dbcb38
JorgeHCamara <jorgehcamara@hotmail.com>
2023-07-21 02:58:53 -0300

    chore: http request delete added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateThoughtsComponent } from './components/thoughts/create-thoughts/create-thoughts.component';
 import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
+import { DeleteThoughtComponent } from './components/thoughts/delete-thought/delete-thought.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: ListThoughtsComponent
+  },
+  {
+    path: 'thoughts/deleteThought/:id',
+    component: DeleteThoughtComponent
   }
 ];
 
