872b10a74ba859e4667a0608147fd7beb469d908
JorgeHCamara <jorgehcamara@hotmail.com>
2023-07-21 01:15:58 -0300

    chore: home ng-template 0 thoughts added

--- src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
+++ src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
@@ -8,16 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListThoughtsComponent implements OnInit {
 
   thoughtsList = [
-    {
-      content: 'Passo informações para o componente filho',
-      author: 'Componente pai',
-      template: 'template3',
-    },
-    {
-      content: 'Minha propriedade é decorada com @Input()',
-      author: 'Componente filho',
-      template: 'template2',
-    }
+
   ];
 
   constructor () {}
