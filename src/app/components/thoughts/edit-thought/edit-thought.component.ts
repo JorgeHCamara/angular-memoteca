9868865da46dfa885fcc61a62fc4ab0fbab74b00
JorgeHCamara <jorgehcamara@hotmail.com>
2023-07-28 18:59:11 -0300

    chore: edit-component reactive form update

--- src/app/components/thoughts/edit-thought/edit-thought.component.ts
+++ src/app/components/thoughts/edit-thought/edit-thought.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Thought } from '../thought';
 import { ThoughtService } from '../thought.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-thought',
@@ -10,34 +11,49 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EditThoughtComponent implements OnInit {
 
-  thought: Thought = {
-    id: 0,
-    content: '',
-    author: '',
-    template: '',
-  }
+  form!: FormGroup;
 
   constructor(
     private service: ThoughtService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
     ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.service.searchById(parseInt(id!)).subscribe((thought) => {
-      this.thought = thought;
+      this.form = this.formBuilder.group({
+        id: [thought.id],
+        content: [thought.content, Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])],
+        author: [thought.author, Validators.compose([
+          Validators.required,
+          Validators.minLength(3)
+        ])],
+        template: [thought.template]
+      })
     })
   }
 
   editThought() {
-    this.service.editThoughts(this.thought).subscribe(() => {
+    this.service.editThoughts(this.form.value).subscribe(() => {
       this.router.navigate(['/home'])
     })
   }
 
   returnHomePage() {
     this.router.navigate(['/home'])
+  }
+
+  enableButton() {
+    if (this.form.valid) {
+      return 'button'
+    } else {
+      return 'disabled__button'
+    }
   }
 
 }
